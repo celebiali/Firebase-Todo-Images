0936b7b4ee4e5c427abc3899eb1db257dd854fe7
Ali Rıza ÇELEBİ <ali1999_@hotmail.com>
2024-05-22 16:26:27 +0300

    Update Title.jsx

--- src/components/AddArticle/Title.jsx
+++ src/components/AddArticle/Title.jsx
@@ -3,13 +3,15 @@ import TextField from "@mui/material/TextField";
 
 export default function Title({ onChange, value }) {
   return (
-    <TextField
-      onChange={onChange}
-      name="title"
-      defaultValue="New Title"
-      placeholder="New Title"
-      className="title-input"
-      size="small"
-    />
+    <div className="title">
+      <TextField
+        onChange={onChange}
+        name="title"
+        defaultValue="New Title"
+        placeholder="New Title"
+        className="title-input"
+        size="small"
+      />
+    </div>
   );
 }
